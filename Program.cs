db3b11727e1f6531926a0a606fcfcb57276c12eb
Agnes Davidsdottir <agnes.davidsdottir@hiq.se>
2023-10-20 11:30:01 +0200

    add methods to test main method :)

--- Program.cs
+++ Program.cs
@@ -5,19 +5,37 @@ class Program
 
     static void Main(string[] args)
     {
-
         try
         {
             AlphaBot ab = new AlphaBot();
             
             ab.Forward();
             
-            Console.Write("Moving! :D\n");
+            Console.Write("Moving Forward! :D\n");
+
+            Thread.Sleep(1000);
+
+            ab. Left(); 
+
+            Console.Write("Left! :D\n");
+
+            Thread.Sleep(1000);
+
+            ab.Backward(); 
+
+            Console.Write("Backward! :D\n");
+
+            Thread.Sleep(1000);
+
+            ab.Right(); 
+
+            Console.Write("Right! :D\n");
 
             Thread.Sleep(1000);
 
             ab.Stop();
 
+            Console.Write("Stop! :D\n");
         }
         catch(Exception e)
         {
