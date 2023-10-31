b347de999457a94ee41b085232127b64485ef2c7
Wilma Axelsson <wilax@telia.com>
2023-10-31 09:00:19 +0100

    first push

--- Program.cs
+++ Program.cs
@@ -5,35 +5,7 @@ class Program
 
     static void Main(string[] args)
     {
-        try
-        {
-            AlphaBot ab = new AlphaBot();
-            
-            ab.Forward();
-            Console.Write("Moving Forward! :D\n");
-            Thread.Sleep(1000);
 
-            //ab. Left();
-            //Console.Write("Left! :D\n"); 
-            //Thread.Sleep(1000);
-
-            //ab.Backward();
-            //Console.Write("Backward! :D\n");
-            //Thread.Sleep(1000);
-
-            ab.Right(); 
-            Console.Write("Right! :D\n");
-            Thread.Sleep(1000);
-
-            ab.Stop();
-
-            Console.Write("Stop! :D\n");
-        }
-        catch(Exception e)
-        {
-            Console.Write("Ajjjdåååå D: " + e.Message);
-        }
         
-        Console.Write("Done! \n");  
     }
 }
