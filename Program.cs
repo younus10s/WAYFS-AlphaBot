60de1bc295e1ab8340e9600334287ea368d48581
Filip Alberius <Filip.Alberius@hiq.se>
2023-11-23 15:48:42 +0100

    Lint Program.cs

--- Program.cs
+++ Program.cs
@@ -2,50 +2,48 @@ using System.Net.WebSockets;
 
 namespace ConsoleApplication
 {
-class Program
+    class Program
     {
-
-    //public static WebSocketHandler webSocketHandler = new WebSocketHandler();
 
         static async Task Main(string[] args)
         {
-
-        double Power = 0.4;
-        bool Calibrate = true;
-        int Rows = 5;
-        int Cols = 5;
-
-        Console.WriteLine("TEST");
-        //GridBot Gunnar = new GridBot(Power, Calibrate, Rows, Cols);
-        //TxtParser TParser = new TxtParser();
-
-        //await TParser.RunFile("robot.txt", Gunnar); 
-
-        //AppCmdParser cmdParser = new AppCmdParser(Gunnar);
+            double Power = 0.4;
+            bool Calibrate = true;
+            int Rows = 5;
+            int Cols = 5;
             
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+            // public static WebSocketHandler webSocketHandler = new WebSocketHandler();
 
-        app.UseWebSockets();
+            // GridBot Gunnar = new GridBot(Power, Calibrate, Rows, Cols);
+            // TxtParser TParser = new TxtParser();
 
-        app.Use(async (context, next) =>
-        {
-            if (context.WebSockets.IsWebSocketRequest)
+            // await TParser.RunFile("robot.txt", Gunnar); 
+
+            // AppCmdParser cmdParser = new AppCmdParser(Gunnar);
+            
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseWebSockets();
+
+            app.Use(async (context, next) =>
             {
-                WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                Console.WriteLine("The frontend is connected");
-                WebSocketHandler webSocketHandler = new WebSocketHandler(webSocket);
-                //await webSocketHandler.HandleWebSocketAsync(cmdParser);
-                await webSocketHandler.HandleWebSocketAsync();
-                Console.WriteLine("after socket messages");
-            }
-            else
-            {
-                await next();
-            }
-        });
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    Console.WriteLine("The frontend is connected");
+                    WebSocketHandler webSocketHandler = new WebSocketHandler(webSocket);
+                    //await webSocketHandler.HandleWebSocketAsync(cmdParser);
+                    await webSocketHandler.HandleWebSocketAsync();
+                    Console.WriteLine("after socket messages");
+                }
+                else
+                {
+                    await next();
+                }
+            });
 
-        await app.RunAsync();
+            await app.RunAsync();
         }
     }
 }
