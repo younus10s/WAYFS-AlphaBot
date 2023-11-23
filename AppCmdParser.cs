02ac83f4d264579039ff77065294909c2bd89a9b
Filip Alberius <Filip.Alberius@hiq.se>
2023-11-23 13:47:52 +0100

    Lint AppCmdParser.cs

--- AppCmdParser.cs
+++ AppCmdParser.cs
@@ -13,30 +13,42 @@
  * ExecuteCommand() 
  * Find words and run the corresponding method for GridBot.  
  */
-public class AppCmdParser {
+public class AppCmdParser
+{
     public GridBot Gunnar; 
 
-    public AppCmdParser(GridBot gridBot) {
+    public AppCmdParser(GridBot gridBot)
+    {
         Gunnar = gridBot;
     }
 
 
-    public async Task RunCommand(string command) {
+    public async Task RunCommand(string command)
+    {
 
         string[] Parts = command.Split(',');
         
-        if(Parts[0] == "PLACE")
-            Gunnar.Place(int.Parse(Parts[1]), int.Parse(Parts[2]), Parts[3].ToLower());
-        else if(Parts[0] == "MOVE")
-            Gunnar.Move();
-        else if(Parts[0] == "LEFT")
-            Gunnar.Left();
-        else if(Parts[0] == "RIGHT")
-            Gunnar.Right();
-        else if(Parts[0] == "REPORT")
-            await Gunnar.Report();
-        else 
-            Console.WriteLine("Invalid Command OTHER");
+        switch (Parts[0])
+        {
+            default:
+                Console.WriteLine("Invalid Command OTHER");
+                break;
+            case "PLACE":
+                Gunnar.Place(int.Parse(Parts[1]), int.Parse(Parts[2]), Parts[3].ToLower());
+                break;
+            case "MOVE":
+                Gunnar.Move();
+                break;
+            case "LEFT":
+                Gunnar.Left();
+                break;
+            case "RIGHT":
+                Gunnar.Right();
+                break;
+            case "REPORT":
+                await Gunnar.Report();
+                break;
+        }
     }
     
 }
