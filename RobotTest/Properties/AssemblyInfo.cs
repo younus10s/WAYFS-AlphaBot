2e3875951e35aaafd2dc76fc6fb54a829bbb7fcb
Filip Alberius <Filip.Alberius@hiq.se>
2023-11-28 11:02:31 +0100

    Added test project for Robot to solution

---
+++ RobotTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RobotTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HiQ")]
+[assembly: AssemblyProduct("RobotTest")]
+[assembly: AssemblyCopyright("Copyright © HiQ 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3f03b25-84c7-47d1-bb98-73d603ff6af0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
