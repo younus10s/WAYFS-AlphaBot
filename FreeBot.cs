3f1effc7fdcdf54d51aa3ea6bda8ac6171405c4d
Younus <yousa991@student.liu.se>
2023-11-30 13:26:49 +0100

    free movement ready to test

--- FreeBot.cs
+++ FreeBot.cs
@@ -8,7 +8,24 @@ public class FreeBot : AlphaBot
         //Maybe add more variable ints
     }
 
-    public void Move(double dx_, double dy_){
+    public void Move(double dx, double dy){
+        double Abs = Math.Sqrt(dx * dx + dy * dy);
+        double Ang = Math.Atan2(dy, dx);
+
+        double Scaled_abs = Math.Min(Abs, 0.5);
+
+        double[] powers = CalculatePower(Scaled_abs, Ang);
+
+        if(dy > 0)
+            MotionControl.Forward(Scaled_abs);   
+        else if(dy < 0)
+            MotionControl.Backward(Scaled_abs);
+
+        MotionControl.SetPowerLeft(powers[0]);
+        MotionControl.SetPowerRight(powers[1]);
+
+
+        /*
         double dx = dx_ * 0.80;
         double dy = dy_ * 0.80;
         double abs = Math.Sqrt(dx * dx + dy * dy);
@@ -34,7 +51,23 @@ public class FreeBot : AlphaBot
             }else{
                 MotionControl.Stop();
             }
-        }
+        }*/
+    }
+
+
+    public double[] CalculatePower(double abs, double angle){
+        double[] powers = new double[2];
+
+        powers[0] = abs * (1 + Math.Cos(angle));
+        powers[1] = abs * (1 - Math.Cos(angle));
+
+        //Normalize motor powers if they exceed 1
+        double max_power = Math.Max(powers[0], powers[1]);
+        max_power = Math.Max(max_power, 1);
+        powers[0] /= max_power;
+        powers[1] /= max_power;
+
+        return powers;
     }
 
 }
